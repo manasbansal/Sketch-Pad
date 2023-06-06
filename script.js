986c1d505ba0a383a815f95e0aaba7c0eccb41da
Manas <manasbansal12@gmail.com>
2023-06-05 23:14:53 -0700

    created 16x16 grid using js and flexbox

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector("#container");
+
+for (let i = 0; i < 273; i++) {
+    let grid = document.createElement("div");
+    container.appendChild(grid);    
+}
+
+
